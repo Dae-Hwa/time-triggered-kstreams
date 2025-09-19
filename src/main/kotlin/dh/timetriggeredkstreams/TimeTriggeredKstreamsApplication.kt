4b5d31d13bbafe1e6b1534fd56f9374bc56a8c9c
Dae-Hwa Jeong <dae_hwa@deali.net>
2025-09-20 00:35:08 +0900

    init

---
+++ src/main/kotlin/dh/timetriggeredkstreams/TimeTriggeredKstreamsApplication.kt
@@ -0,0 +1,11 @@
+package dh.timetriggeredkstreams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeTriggeredKstreamsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TimeTriggeredKstreamsApplication>(*args)
+}
---
+++ src/test/kotlin/dh/timetriggeredkstreams/TimeTriggeredKstreamsApplicationTests.kt
@@ -0,0 +1,15 @@
+package dh.timetriggeredkstreams
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class TimeTriggeredKstreamsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
